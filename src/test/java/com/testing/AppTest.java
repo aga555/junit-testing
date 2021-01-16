d1e60a039d0b7c6b41ca58c0d138ec8f7dfac4b5
akrawczyk <agammkrawczyk@gmail.com>
2021-01-16 18:53:55 +0100

    add mockito

---
+++ src/test/java/com/testing/AppTest.java
@@ -0,0 +1,15 @@
+package com.testing;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Calendar;
+
+public class AppTest {
+
+    @Test
+    public void testMockito() {
+        Calendar mockCalendar = Mockito.mock(Calendar.class);
+
+    }
+}
