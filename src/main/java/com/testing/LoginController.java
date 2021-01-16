fa4f418a35940333b0b51d4afa83d910bd1e276b
akrawczyk <agammkrawczyk@gmail.com>
2021-01-16 19:05:24 +0100

    refactor controller method

--- src/main/java/com/testing/LoginController.java
+++ src/main/java/com/testing/LoginController.java
@@ -3,14 +3,7 @@ package com.testing;
 public class LoginController {
     private AuthenticationService service;
 
-    public String service(String username, String password)
-    {
-        if (service.authenticate(username, password)) {
-            return "/homee";
-        } else {
-            return "/login";
-        }
-
+    public String service(String username, String password) {
+        return service.authenticate(username, password) ? "/homee" : "/login";
     }
-
 }
